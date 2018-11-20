0504a7dfb491f990d8900e7c574ed82d3dcbd2bf
Will <doggy.huang@gmail.com>
2018-11-20 11:09:45 +0800

    刪除用不到的元件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Page1Component } from './page1/page1.component';
-import { Page2Component } from './page2/page2.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Page1Component,
-    Page2Component,
     NotFoundComponent
   ],
   imports: [
