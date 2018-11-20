3620cc4b7e2df4d48b744314e132b1fe2f2826c3
Will <doggy.huang@gmail.com>
2018-11-20 10:47:53 +0800

    練習不同的路由類型

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Page1Component } from './page1/page1.component';
 import { Page2Component } from './page2/page2.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     Page1Component,
-    Page2Component
+    Page2Component,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
