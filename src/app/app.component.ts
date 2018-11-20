9b958b01009a3eae14ed20b23d9cd70595e0502f
Will <doggy.huang@gmail.com>
2018-11-20 16:31:22 +0800

    示範訂閱 this.router.events 的方法

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng7-advanced';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
+
+  ngOnInit(): void {
+    // this.router.events.subscribe(evt => {});
+  }
 }
