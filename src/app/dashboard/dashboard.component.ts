86b0a8d7727a901ad59b8484c801cb4b143e1501
Will <doggy.huang@gmail.com>
2018-11-20 11:54:38 +0800

    修正 Dashboard 與 3rd-party library 整合的問題

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DashboardInit } from './DashboardInit';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
+    DashboardInit();
   }
-
 }
---
+++ src/typings.d.ts
@@ -0,0 +1,1 @@
+declare var Morris: any;
