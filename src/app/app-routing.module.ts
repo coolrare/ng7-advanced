54be3888b488626f315ccc307515f97f3f4beaf5
Will <doggy.huang@gmail.com>
2018-11-20 17:02:09 +0800

    練習 路由守門員 (Route Guard) 作法

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { fallbackRoute } from './shared/fallbackRoute';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { TablesComponent } from './tables/tables.component';
+import { AuthGuard } from './auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
-  { path: 'tables', component: TablesComponent },
-  { path: 'tables/:num', component: TablesComponent },
+  { path: 'tables', canActivate: [AuthGuard], component: TablesComponent },
+  { path: 'tables/:num', canActivate: [AuthGuard], component: TablesComponent },
   {
     path: 'charts',
     loadChildren: './charts/charts.module#ChartsModule'
