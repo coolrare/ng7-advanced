64a8bf7fa18b9d02e007731f0e688224cd049016
Will <doggy.huang@gmail.com>
2018-11-20 15:24:09 +0800

    練習在同一個路由內變換參數

--- src/app/tables/tables.component.ts
+++ src/app/tables/tables.component.ts
@@ -7,16 +7,20 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./tables.component.css']
 })
 export class TablesComponent implements OnInit {
-  num;
+  num = 0;
   constructor(private router: Router, private route: ActivatedRoute) {}
 
   ngOnInit() {
     this.route.params.subscribe(p => {
-      this.num = p['num'];
+      this.num = +p['num'];
     });
 
     $('#dataTables-example').DataTable({
       responsive: true
     });
+  }
+
+  addOne() {
+    this.router.navigateByUrl('/tables/' + (this.num + 1));
   }
 }
