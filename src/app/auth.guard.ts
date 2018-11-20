39de4691126548881a9c84bb832e1d1f7083245f
Will <doggy.huang@gmail.com>
2018-11-20 17:04:39 +0800

    示範在 AuthGuard 判斷該如何進行下一步

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,14 +1,26 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
-    return !!localStorage['username'];
+    state: RouterStateSnapshot
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    if (!localStorage['username']) {
+      this.router.navigateByUrl('/');
+      return false;
+    } else {
+      return true;
+    }
   }
 }
